7294328365306bad0a29b64b46096d2244d9511e
yiyun <1042559614@qq.com>
2019-09-11 22:28:19 +0800

    添加试卷完善

--- src/views/main/examManager/addExamList/index.tsx
+++ src/views/main/examManager/addExamList/index.tsx
@@ -6,7 +6,8 @@ const ReactMarkdown = require('react-markdown')
 
 
 interface Props{
-  questions:any
+  questions:any,
+  history:any
 }
 
 @inject('examManger')
@@ -16,19 +17,28 @@ class AddExamList extends React.Component<Props>{
   state={
     questions:JSON.parse(window.localStorage.getItem('content')+'') 
   }
+  handClick=()=>{
+    this.props.history.push('/home/exammanager/questionList')
+  }
   render(){
     console.log(this.state)
     return <div>
       <h2 className="adduser-title">创建试卷</h2>
       <div className="content">
         <Button>添加新题</Button>
-        {this.state.questions.length?this.state.questions.map((item:any)=>{
+        {this.state.questions.length?this.state.questions.map((item:any,index:number)=>{
             console.log(item)
-            return <div key={item.questions_id}>
-               <p>{item.title}</p>
+            return <div key={item.questions_id} style={{border:'1px solid #ccc',marginTop:'20px',padding:'20px'}} >
+               <div className="examList-title">
+                  <p>{index+1}:{item.title}</p>
+                  <a href="#">删除</a>
+               </div>
                <ReactMarkdown source={item.questions_stem}></ReactMarkdown>
             </div>
           }):<div>暂无数据</div>}
+      </div>
+      <div className="btn-bigBox">
+      <button className="btn-addExam" onClick={this.handClick}>创建试卷</button>
       </div>
     </div>
   }
