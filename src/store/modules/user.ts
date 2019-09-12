5969e39e3d2acb9d376b3fe92888dbdae578ccb6
SmilePig <306826385@qq.com>
2019-09-12 10:21:12 +0800

    avatar

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,57 +1,67 @@
-import { observable, action } from 'mobx';
-import { getUserLogin, getUserInfoUser, getViewAuthority } from '../../service/index';
-import { setToken, removeToken } from '../../utils/index';
+import { observable, action } from 'mobx'
+import {
+    getUserLogin,
+    getUserInfoUser,
+    getViewAuthority
+} from '../../service/index'
+import { setToken, removeToken } from '../../utils/index'
 
 //获取本地存储的用户信息
-let account = {};
+let account = {}
 
 if (window.localStorage.getItem('account')) {
-	account = JSON.parse(window.localStorage.getItem('account') + '');
+    account = JSON.parse(window.localStorage.getItem('account') + '')
 }
 
 interface LoginInfo {
-	user_name: string;
-	user_pwd: string;
-	remember: any,
-	autoLogin: any
+    user_name: string
+    user_pwd: string
+    remember: any
+    autoLogin: any
 }
 
 class User {
-	@observable isLogin: boolean = false;
-	@observable account: any = account;
-	@observable userInfo: any = {};
-	@observable viewAuthority: object[] = [];
+    @observable isLogin: boolean = false
+    @observable account: any = account
+    @observable userInfo: any = {}
+    @observable viewAuthority: object[] = []
+    @observable avatar: string = ''
 
-	@action async login(form: LoginInfo): Promise<any> {
-		const result: any = await getUserLogin(form);
-		if (result.code === 1) {
-			//判断是否记住密码
-			if (form.remember) {
-				window.localStorage.setItem('account', JSON.stringify(form));
-			} else {
-				window.localStorage.removeItem('account');
-			}
-			if (form.autoLogin) {
-				setToken(result.token);
-			}
-		}
-		return result;
-	}
-	//退出登录
-	@action async loginout(): Promise<any> {
-		removeToken()
-	}
-	//获取用户信息
-	@action async getUserInfoUser(): Promise<any> {
-		let userInfo: any = await getUserInfoUser();
-		this.userInfo = userInfo.data;
-		this.getViewAuthority()
-	}
-	//获取用户权限
-	@action async getViewAuthority(): Promise<any> {
-		let viewAuthority: any = await getViewAuthority()
-		this.viewAuthority = viewAuthority.data
-	}
+    @action async login(form: LoginInfo): Promise<any> {
+        const result: any = await getUserLogin(form)
+        if (result.code === 1) {
+            //判断是否记住密码
+            if (form.remember) {
+                window.localStorage.setItem('account', JSON.stringify(form))
+            } else {
+                window.localStorage.removeItem('account')
+            }
+            if (form.autoLogin) {
+                setToken(result.token)
+            }
+        }
+        return result
+    }
+    //退出登录
+    @action async loginout(): Promise<any> {
+        removeToken()
+    }
+    //获取用户信息
+    @action async getUserInfoUser(): Promise<any> {
+        let userInfo: any = await getUserInfoUser()
+        this.userInfo = userInfo.data
+        this.getViewAuthority()
+    }
+    //获取用户权限
+    @action async getViewAuthority(): Promise<any> {
+        let viewAuthority: any = await getViewAuthority()
+        this.viewAuthority = viewAuthority.data
+    }
+
+    // 修改用户头像
+    @action changeAvatar(avatar: string): void {
+        this.avatar = avatar
+    }
 }
 
-export default User;
+export default User
