a041a7e40adc2d8fea88cfd8abece82ab4eaeb55
yiyun <1042559614@qq.com>
2019-09-12 10:20:57 +0800

    路由列表完善

---
+++ src/views/main/Confirmauthority/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+class Confirmauthority extends React.Component{
+  render(){
+    return (
+      <div>
+        403页面
+      </div>
+    )
+  }
+}
+export default Confirmauthority
